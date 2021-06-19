5c10b4954f0e738fdb0dea64cfed06b263b0cfae
Maximilian Alexander <max@ditto.live>
2021-06-19 10:41:18 -0400

    completing implementation

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,65 @@ import Ditto, {
   SortDirection,
   Subscription,
   UpdateResult,
+  RemotePeer,
+  Observer,
+  SingleObservationHandler,
 } from "@dittolive/ditto";
 
 import { Observable } from "rxjs";
+
+export class RxPendingIDSpecificOperation
+  implements PendingIDSpecificOperation
+{
+  op: PendingIDSpecificOperation;
+
+  constructor(op: PendingIDSpecificOperation) {
+    this.op = op;
+  }
+  evict(): Promise<boolean> {
+    return this.op.evict();
+  }
+  exec(): Promise<Document> {
+    return this.op.exec();
+  }
+  update(closure: (document: any) => void): Promise<UpdateResult[]> {
+    return this.op.update(closure);
+  }
+  then<TResult1 = DocumentValue, TResult2 = never>(
+    onfulfilled?: (value: DocumentValue) => TResult1 | PromiseLike<TResult1>,
+    onrejected?: (reason: any) => TResult2 | PromiseLike<TResult2>
+  ): PromiseLike<TResult1 | TResult2> {
+    return this.op.then(onfulfilled, onrejected);
+  }
+
+  subscribe(): Subscription {
+    return this.op.subscribe();
+  }
+  observe(handler: SingleObservationHandler): Promise<LiveQuery> {
+    return this.op.observe(handler);
+  }
+  remove(): Promise<boolean> {
+    return this.op.remove();
+  }
+
+  get document$(): Observable<Document | undefined> {
+    return new Observable((subscriber) => {
+      let liveQuery: LiveQuery | undefined;
+
+      this.op
+        .observe((doc) => {
+          subscriber.next(doc);
+        })
+        .then((lq) => {
+          liveQuery = lq;
+        });
+
+      return () => {
+        liveQuery?.stop();
+      };
+    });
+  }
+}
 
 export class RxPendingCursorOperation implements PendingCursorOperation {
   private op: PendingCursorOperation;
@@ -23,45 +79,48 @@ export class RxPendingCursorOperation implements PendingCursorOperation {
     this.op = op;
   }
 
-  sort(propertyPath: string, direction?: SortDirection): PendingCursorOperation;
-  sort(propertyPath: string): PendingCursorOperation;
-  sort(propertyPath: any, direction?: any): PendingCursorOperation {
-    throw new Error("Method not implemented.");
+  sort(
+    propertyPath: string,
+    direction?: SortDirection
+  ): RxPendingCursorOperation;
+  sort(propertyPath: string): RxPendingCursorOperation;
+  sort(propertyPath: any, direction?: any): RxPendingCursorOperation {
+    return new RxPendingCursorOperation(this.op.sort(propertyPath, direction));
   }
   offset(count: number): PendingCursorOperation {
-    throw new Error("Method not implemented.");
+    return new RxPendingCursorOperation(this.op.offset(count));
   }
   limit(count: number): PendingCursorOperation {
-    throw new Error("Method not implemented.");
+    return new RxPendingCursorOperation(this.op.limit(count));
   }
   subscribe(): Subscription {
-    throw new Error("Method not implemented.");
+    return this.op.subscribe();
   }
   observe(handler: QueryObservationHandler): Promise<LiveQuery> {
-    throw new Error("Method not implemented.");
+    return this.op.observe(handler);
   }
   remove(): Promise<DocumentID[]> {
-    throw new Error("Method not implemented.");
+    return this.op.remove();
   }
   evict(): Promise<DocumentID[]> {
-    throw new Error("Method not implemented.");
+    return this.op.evict();
   }
   exec(): Promise<Document[]> {
-    throw new Error("Method not implemented.");
+    return this.op.exec();
   }
   update(
     closure: (documents: [any]) => void
   ): Promise<{ [documentID: string]: UpdateResult[] }> {
-    throw new Error("Method not implemented.");
+    return this.op.update(closure);
   }
   then<TResult1 = DocumentValue[], TResult2 = never>(
     onfulfilled?: (value: DocumentValue[]) => TResult1 | PromiseLike<TResult1>,
     onrejected?: (reason: any) => TResult2 | PromiseLike<TResult2>
   ): PromiseLike<TResult1 | TResult2> {
-    throw new Error("Method not implemented.");
+    return this.op.then(onfulfilled, onrejected);
   }
 
-  toObservable(): Observable<Document[]> {
+  get documents$(): Observable<Document[]> {
     return new Observable((subscriber) => {
       let liveQuery: LiveQuery | undefined;
       this.op
@@ -99,8 +158,10 @@ export class RxCollection implements Collection {
     );
   }
 
-  findByID(id: DocumentID): PendingIDSpecificOperation {
-    return this.store.collection(this.name).findByID(id);
+  findByID(id: DocumentID): RxPendingIDSpecificOperation {
+    return new RxPendingIDSpecificOperation(
+      this.store.collection(this.name).findByID(id)
+    );
   }
 
   insert(value: DocumentToInsert): Promise<DocumentID> {
@@ -126,6 +187,10 @@ export class RxStore implements Store {
 export default class RxDitto {
   ditto: Ditto;
 
+  get siteID(): number | BigInt {
+    return this.ditto.siteID;
+  }
+
   constructor(ditto: Ditto) {
     this.ditto = ditto;
   }
@@ -133,16 +198,35 @@ export default class RxDitto {
   get store(): RxStore {
     return new RxStore(this.ditto);
   }
-  
+
   startSync() {
-    this.ditto.startSync()
+    this.ditto.startSync();
   }
-  
+
   stopSync() {
-    this.ditto.stopSync()
+    this.ditto.stopSync();
   }
-  
+
   setAccessLicense(token: string) {
-    this.ditto.setAccessLicense(token)
+    this.ditto.setAccessLicense(token);
+  }
+
+  /**
+   * An RxJS Observable of RemotePeer[]
+   */
+  get peers$(): Observable<RemotePeer[]> {
+    return new Observable((subscriber) => {
+      let obs: Observer | undefined;
+      this.ditto
+        .observePeers((remotePeers) => {
+          subscriber.next(remotePeers);
+        })
+        .then((o) => {
+          obs = o;
+        });
+      return () => {
+        obs?.stop();
+      };
+    });
   }
 }
